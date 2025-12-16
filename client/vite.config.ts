26621c6aae120e21088b9212efb005ec6df04e7a
sahil <shp2382@g.rit.edu>
2025-12-15 22:17:19 -0500

    removed remaining vexflow instances in client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,4 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  optimizeDeps: {
-    include: ['vexflow']
-  }
 })
